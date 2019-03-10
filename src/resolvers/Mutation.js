f4b6e7615f42425fd64edb80016aa06526850cc6
Vinh Le <vinh@vertics.co>
2019-03-10 08:59:28 +0200

    Fix some stuffs

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+/**
+ * Resolvers for Mutations
+ *
+ * @author Vinh Le <lethanhvinh95@gmail.com>
+ *
+ */
 
-module.exports = mutations;
+const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		const item = await ctx.db.mutation.createItem({
+			data: {
+				...args
+			}
+		}, info)
+
+		return item
+
+	}
+
+};
+
+module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+	async items(parent, args, ctx, info) {
+		const items = await ctx.db.query.items()
+		console.log(items)
+		return items
+	}
+};
 
 module.exports = Query;
