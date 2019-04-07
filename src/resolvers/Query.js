babf3bdd2188eab7468d47ee1f592f20e1919830
Vinh Le <vinh@vertics.co>
2019-04-07 09:34:35 +0300

    refactor: use prisma-binding and add itemConnection query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,8 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-	async items(parent, args, ctx, info) {
-		const items = await ctx.db.query.items()
-		console.log(items)
-		return items
-	}
+	items: forwardTo('db'),
+	itemsConnection: forwardTo('db'),
 };
 
 module.exports = Query;
